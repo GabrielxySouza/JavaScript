d6ab2a02534350d0527660a3c9b1f6ec83c1a005
Gabriel Souza <gabrielxysouza@gmail.com>
2023-11-15 09:35:05 -0300

    ex08, condição simples

---
+++ aula11/ex08.js
@@ -0,0 +1,10 @@
+console.log('O console funcionou corretamente!')
+
+var vel = 55
+console.log(`A velocidade do veículo é de ${vel}km/h`)
+
+if(vel > 60){   //condição simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO')
+}
+
+console.log('Dirija sempre usando capacete ou cinto de segurança!')
