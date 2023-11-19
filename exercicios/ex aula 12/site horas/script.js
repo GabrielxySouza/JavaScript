dd13df9db9731612a08c1b0a247630c91198e353
Gabriel Souza <gabrielxysouza@gmail.com>
2023-11-19 13:35:21 -0300

    adc imagens e chamada para function carregar( )

--- exercicios/ex aula 12/site horas/script.js
+++ exercicios/ex aula 12/site horas/script.js
@@ -1,1 +1,19 @@
-alert("Olá...")
+
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora <12){
+        //Bom dia
+        img.src = 'imagens/img-manhã.png'
+    } else if (hora >= 12 && hora < 18){
+        //Boa trade
+        img.src = 'imagens/img-tarde.png'
+    } else{
+        //Boa noite
+        img.src = 'imagens/img-noite.png'
+    }
+
+}
