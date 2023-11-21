cdb3c5efdf6c872eebe7b9c88ac8857b4fe4dece
Gabriel Souza <gabrielxysouza@gmail.com>
2023-11-21 15:28:18 -0300

    adicionando mensagens e cores de fundos no site

--- exercicios/ex aula 12/site horas/script.js
+++ exercicios/ex aula 12/site horas/script.js
@@ -4,16 +4,22 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    
     if (hora >=0 && hora <12){
         //Bom dia
+        msg.innerHTML = `Bom dia! Agora são ${hora} horas.`
         img.src = 'imagens/img-manhã.png'
+        document.body.style.background = '#FFD75D'
     } else if (hora >= 12 && hora < 18){
         //Boa trade
+        msg.innerHTML = `Boa tarde! Agora são ${hora} horas.`
         img.src = 'imagens/img-tarde.png'
+        document.body.style.background = '#D68B51'
     } else{
         //Boa noite
+        msg.innerHTML = `Boa noite! Agora são ${hora} horas.`
         img.src = 'imagens/img-noite.png'
+        document.body.style.background = '#00113F'
     }
 
 }
