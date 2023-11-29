bdd526096d7c432a81a4f14524f76a1b76e93019
Gabriel Souza <gabrielxysouza@gmail.com>
2023-11-29 19:57:20 -0300

    repetições parte 1

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+//Exemplo de repetições simples (teste lógico no inicio)
+
+var c = 1
+while (c <= 4){
+    console.log('Tudo bem?')
+    c++ //C = c + 1
+}
+
+//contando passos  *repare que ao exercutar (F8) ele não começa do 1, isso porquê os primeiros passos foram o do laço acima...  
+while (c <= 9){
+    console.log(`Passo ${c}`)
+    c++ //C = c + 1
+}
+
+// Estrutura de repetição com teste lógico no final *no terminal a contagem começa em 1 até 6, já que usamos outra var (c2)
+
+var c2 = 1
+do{
+    console.log(`Passo ${c2}`)
+    c2++
+} while (c2 <= 6)
