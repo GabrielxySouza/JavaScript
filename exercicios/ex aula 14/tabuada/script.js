7b2650ce2ba26b3dd79771bbb222d15156b01b91
Gabriel Souza <gabrielxysouza@gmail.com>
2023-12-02 14:27:42 -0300

    js tabuada

--- exercicios/ex aula 14/tabuada/script.js
+++ exercicios/ex aula 14/tabuada/script.js
@@ -1,1 +1,13 @@
-alert("Olá...")
+function gerar(){
+    var num = document.getElementById('inum')
+    num = Number()
+    var tab = document.getElementById('itab')
+    var most = document.getElementById('most')
+    var c = 1
+    tab == num
+    while(c <= 10){
+        most.innerHTML = `${num} x ${c} = ${tab}`
+        tab = num*c
+        c++
+    }
+}
